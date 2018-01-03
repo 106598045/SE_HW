40235235b2cfe446af718f4590ffde83e5b2f858
hanlong <t106598045@ntut.edu.tw>
2018-01-03 20:36:18 +0800

    98498

--- homework4/src/homework4/hw4.java
+++ homework4/src/homework4/hw4.java
@@ -1,8 +1,22 @@
 package homework4;
 
+import java.util.Scanner;
 
 public class hw4 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println(letterGrade(Integer.parseInt(scanner.nextLine())));
+		}
+	}
+	
+	public static char letterGrade(int score) {
+		char grade; if (score <0 || score > 100) grade = 'X'; 
+		else if (score>=90 && score <=100) grade = 'A'; 
+		else if (score>=80 && score <90) grade = 'B'; 
+		else if (score>=70 && score <80) grade = 'C'; 
+		else if (score>=60 && score <70) grade = 'D'; 
+		else grade ='F'; 
+		return grade;  
 	}
 }
